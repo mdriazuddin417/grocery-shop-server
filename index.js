3270a3004abe8bd61c84d127b81b62f2ce8441f9
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-05-03 16:06:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://groceryShop:XmVbPIFjBxxqzYY2@cluster0.657ma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("grocery-shop").collection("product");
+    const feedbackCollection = client.db("grocery-shop").collection("feedback");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.dir);
+app.get("/", (req, res) => {
+  res.send("My first check");
+});
+
+app.listen(port, () => {
+  console.log(port, "Example Runnig Port");
+});
